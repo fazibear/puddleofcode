874162ad16315cfbbd64a9866a80f78fcf515b81
Michał Kalbarczyk <fazibear@gmail.com>
2023-03-16 22:38:18 +0100

    pagination

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,15 +1,19 @@
 module Nanoc::Helpers
   module Pagination
     def add_pagination_for(source, destination, per_page)
-      @items
+      pages = @items
         .find_all(source)
         .map {|item| item.identifier}
         .reverse
         .each_slice(per_page)
-        .with_index
-        .each do |items, index|
+
+      pages_links = pages.with_index.map do |item, index|
+        [index, destination + (index > 0 ? "/#{index}" : "")]
+      end
+
+      pages.with_index.each do |items, index|
           identifier = destination + (index > 0 ? "/#{index}" : "")
-          @items.create('', {items: items, page: index}, identifier)
+          @items.create('', {items: items, page: index, pages: pages_links}, identifier)
       end
     end
   end
